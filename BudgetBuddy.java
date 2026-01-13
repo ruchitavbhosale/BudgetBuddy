5eb1190e227fc4af644bd926cc4582b0a49b8bcb
Ruchita V Bhosale <ruchitavikasbhosale14@gmail.com>
2026-01-13 20:52:42 +0530

    Clean up whitespace in BudgetBuddy.java
    Removed unnecessary blank lines in BudgetBuddy.java

--- BudgetBuddy.java
+++ BudgetBuddy.java
@@ -7,8 +7,6 @@ import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-
-
 public class BudgetBuddy extends JFrame {
 
     private JTextField incomeField, expenseField;
